e94c06a0ad71fd3a4637ba28c0072d28ec7af6db
shibacode <sheilaghmorlan@gmail.com>
2017-07-04 14:02:02 -0700

    Create Variables_Input-Output.py
    pseudo code and code to match JV's 1-Variables-Input-Output.py

---
+++ Variables_Input-Output.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# use python to create file
+# prompt user for string input 
+# print user input
+
+userResponse=raw_input('Type a string since thatis what raw_input uses:')
+
+print userResponse
+
+# print unprompted user input
+
+print "I didn't add a prompt. Type something and hit the enter key."
+userResponse2=raw_input()
+
+# prompt for user input and store input in a variable
+# print sentence using user input
+
+myPrompt="Type your name:"
+some-other_response=raw_input(myPrompt)
+print 'You said: '+some_other_response+'likes',steakAndFries
+
+print'''
